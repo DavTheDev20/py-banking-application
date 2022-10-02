b3d0efececd993013537886695150d4776822662
DavTheDev20 <reiddavin@outlook.com>
2022-10-01 20:01:49 -0400

    Add backend user registration logic connected to SQLite database

--- server.py
+++ server.py
@@ -1,10 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request, g
 import sqlite3
 from datetime import datetime
+import traceback
 
 app = Flask(__name__)
 
+SQL_DATABASE = "./db/main.db"
+
 current_date = datetime.now().strftime("%Y")
+
+con = sqlite3.connect(SQL_DATABASE, check_same_thread=False)
+cur = con.cursor()
+
+
+def create_users_table():
+    """Creates SQL database table to store users"""
+    sql = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, email TEXT, password TEXT)"
+    cur.execute(sql)
+
+
+create_users_table()
 
 
 @app.route('/')
@@ -15,6 +30,23 @@ def home():
 @app.route('/register')
 def register():
     return render_template('register.html', curr_date=current_date, title="Register")
+
+
+@app.route('/api/register', methods=["POST"])
+def register_user():
+    data = request.json
+    try:
+        if data["username"] and data["email"] and data["password"]:
+            sql = f"INSERT INTO users (username, email, password) VALUES ('{data['username']}', '{data['email']}', '{data['password']}')"
+            try:
+                cur.execute(sql)
+                con.commit()
+                return {"success": True}
+            except:
+                return {"success": False, "error": traceback.format_exc()}
+    except KeyError:
+        return {"success": False, "error": "Please include username, email, and password in request json body."}, 400
+
 
 @app.route('/user/<user_id>')
 def user_page(user_id):
